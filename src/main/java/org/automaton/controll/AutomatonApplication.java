e1f52020d90cae43cd4de703e26f89e386cad8e6
DemianovE <work.demianov@gmail.com>
2025-07-07 17:06:34 +0200

    add application engine start
    add the engine start depending on the buttons binding

--- src/main/java/org/automaton/controll/AutomatonApplication.java
+++ src/main/java/org/automaton/controll/AutomatonApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import org.automaton.controll.game.GameEngine;
 import org.automaton.controll.game.GameStatus;
 import org.automaton.controll.model.GameConfigModel;
 import org.automaton.controll.ui.ConfigurationPanel;
@@ -22,24 +23,29 @@ public class AutomatonApplication extends Application {
         ConfigurationPanel configurationPanel = ConfigurationPanel.getInstance();
         GameFieldPanel gameFieldPanel = GameFieldPanel.getInstance();
         GameConfigModel model = GameConfigModel.getInstance();
+        GameEngine gameEngine = GameEngine.getInstance();
 
         root.setLeft(configurationPanel);
         root.setCenter(gameFieldPanel);
 
         configurationPanel.getStartButton().setOnAction(e -> {
             model.setGameStatus(GameStatus.RUNNING);
+            gameEngine.startGame();
         });
 
         configurationPanel.getPauseButton().setOnAction(e -> {
             if (model.getGameStatus().get().equals(GameStatus.RUNNING)) {
                 model.setGameStatus(GameStatus.PAUSED);
+                gameEngine.cancelCurrentTimer();
             } else {
                 model.setGameStatus(GameStatus.RUNNING);
+                gameEngine.startGame();
             }
         });
 
         configurationPanel.getResetButton().setOnAction(e -> {
             model.setGameStatus(GameStatus.STOPED);
+            gameEngine.resetGame();
         });
 
 
