9d82ba8e4a04337a687f34ede4ddd784dc0bc9b3
DemianovE <87388931+DemianovE@users.noreply.github.com>
2025-07-11 15:48:47 +0200

    Update GameFieldPanel.java
    1) redraw logic change
    2) add click handling for manual mode
    3) change of the binding logic to not create loops

--- src/main/java/org/automaton/control/ui/GameFieldPanel.java
+++ src/main/java/org/automaton/control/ui/GameFieldPanel.java
@@ -10,8 +10,14 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import org.automaton.control.enums.GameStatus;
+import org.automaton.control.enums.InputType;
 import org.automaton.control.factories.UiComponentFactory;
+import org.automaton.control.game.GameEngine;
+import org.automaton.control.game.GridDrawingMetrics;
 import org.automaton.control.model.GameConfigModel;
+
+import javafx.scene.input.MouseEvent;
 
 /**
  * Main function to make the UI component of game field
@@ -25,6 +31,10 @@ public class GameFieldPanel extends BorderPane {
     private static final GameFieldPanel INSTANCE = new GameFieldPanel();
 
     private final GameConfigModel model = GameConfigModel.getInstance();
+    private final ConfigurationPanel  configurationPanel = ConfigurationPanel.getInstance();
+    private final GameEngine gameEngine = GameEngine.getInstance();
+
+    private GridDrawingMetrics gridMetrics;
 
     private GameFieldPanel(){
         this.setStyle("-fx-background-color: #444;");
@@ -59,13 +69,50 @@ public class GameFieldPanel extends BorderPane {
         this.gameCanvas.widthProperty().bind(canvasContainer.widthProperty());
         this.gameCanvas.heightProperty().bind(canvasContainer.heightProperty());
 
-        this.model.getRows().addListener((obs, oldVal, newVal) -> redraw());
-        this.model.getCols().addListener((obs, oldVal, newVal) -> redraw());
-        this.model.getLivePercent().addListener((obs, oldVal, newVal) -> redraw());
-        this.model.getEpochCount().addListener((obs, oldVal, newVal) -> redraw());
+        gameCanvas.addEventHandler(MouseEvent.MOUSE_CLICKED, this::handleCanvasClick);
 
-        this.gameCanvas.widthProperty().addListener((obs, oldVal, newVal) -> redraw());
-        this.gameCanvas.heightProperty().addListener((obs, oldVal, newVal) -> redraw());
+        this.model.getRows().addListener((obs, oldVal, newVal) -> {
+            this.model.reshapeDataGrip();
+            this.model.resetDataGrid();
+            redraw();
+        });
+        this.model.getCols().addListener((obs, oldVal, newVal) -> {
+            this.model.reshapeDataGrip();
+            this.model.resetDataGrid();
+            redraw();
+        });
+
+
+        this.model.getLivePercent().addListener((obs, oldVal, newVal) -> {
+            this.model.resetDataGrid();
+            redraw();
+        });
+        this.model.getEpochCount().addListener((obs, oldVal, newVal) -> redraw());
+        this.model.getGridData().addListener((obs, oldVal, newVal) -> redraw());
+
+        this.model.getGameInputType().addListener((obs, oldVal, newVal) -> {
+            if (newVal == InputType.MANUAL) { this.model.resetDataToZero(); }
+            else {
+                this.model.resetDataGrid();
+                redraw();
+            }
+        });
+
+        this.gameCanvas.widthProperty().addListener((obs, oldVal, newVal) -> {
+            this.model.reshapeDataGrip();
+            this.model.resetDataGrid();
+            redraw();
+        });
+        this.gameCanvas.heightProperty().addListener((obs, oldVal, newVal) -> {
+            this.model.reshapeDataGrip();
+            this.model.resetDataGrid();
+            redraw();
+        });
+
+        this.configurationPanel.getRandomiseButton().setOnAction(e -> {
+            this.model.resetDataGrid();
+            redraw();
+        });
 
         redraw();
         return canvasContainer;
@@ -97,55 +144,86 @@ public class GameFieldPanel extends BorderPane {
     }
 
     /**
+     * The function to handle the manual grid click
+     * @param event - the click itself
+     */
+    private void handleCanvasClick(MouseEvent event){
+        if (this.model.getGameStatus().get() != GameStatus.STOPED ||
+                this.model.getGameInputType().get() != InputType.MANUAL
+            ) { return; }
+
+        double mouseX =  event.getX();
+        double mouseY = event.getY();
+
+        double clickXInGrid = mouseX - this.gridMetrics.offsetX();
+        double clickYInGrid = mouseY - this.gridMetrics.offsetY();
+
+        if (clickXInGrid >= 0 && clickYInGrid < this.gridMetrics.totalGridWidth() &&
+            clickYInGrid >= 0 && clickXInGrid < this.gridMetrics.totalGridHeight()) {
+
+            int clickCol = (int) (clickXInGrid / this.gridMetrics.cellSize());
+            int clickRow = (int) (clickYInGrid / this.gridMetrics.cellSize());
+
+            if (clickRow < this.gridMetrics.rows() && clickRow >= 0 &&
+                    clickCol < this.gridMetrics.cols() &&  clickCol >= 0) {
+
+                this.model.toggleDataGridCoordinate(clickRow, clickCol);
+            }
+        }
+
+        redraw();
+    }
+
+    /**
      * The main "Magic" of the dynamic grid creation and update. Is used to make the UI actually do its work
      */
     private void redraw(){
         // generate when changing the size of the grid
+
         if (this.model.getEpochCountPrimitive() == 0) {
-            this.model.reshapeDataGrip();
-            this.model.resetDataGrid();
+            // the reset of the basic metrics calculation
+            this.gridMetrics = GridDrawingMetrics.calculate(
+                    this.model.getRowsPrimitive(),
+                    this.model.getColsPrimitive(),
+                    this.gameCanvas.getWidth(),
+                    this.gameCanvas.getHeight());
         }
 
-        int rows = this.model.getRowsPrimitive();
-        int cols = this.model.getColsPrimitive();
-
-        // basically, we delete grid and re-draw each time
+        // basically, we delete the grid and re-draw each time
         this.gc.clearRect(0, 0, this.gameCanvas.getWidth(), this.gameCanvas.getHeight());
 
-        double cellWidth = this.gameCanvas.getWidth() / cols;
-        double cellHeight = this.gameCanvas.getHeight() / rows;
-
-        // we make squares to fit into the grid. SO need to make with smaller from two options
-        double cellSize = Math.min(cellWidth, cellHeight);
-
-        double totalGridWidth = cellSize * cols;
-        double totalGridHeight = cellSize * rows;
-
-        // fancy way of making the grid be centered by making coordinate always considers the offset from the side
-        double offsetX = (this.gameCanvas.getWidth() - totalGridWidth) / 2.0;
-        double offsetY = (this.gameCanvas.getHeight() - totalGridHeight) / 2.0;
-
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
+        for (int r = 0; r < this.gridMetrics.rows(); r++) {
+            for (int c = 0; c < this.gridMetrics.cols(); c++) {
                 // here the live dead logic will be implemented, for tests we make just white
 
                 if (this.model.getDataGridCoordinate(r, c) == 1) {
                     this.gc.setFill(Color.BLACK);
                 } else { this.gc.setFill(Color.WHITE); }
 
-                this.gc.fillRect(offsetX + c * cellSize, offsetY + r * cellSize, cellSize, cellSize); // remember about offset, when calculation
+                this.gc.fillRect(
+                        this.gridMetrics.offsetX() + c * this.gridMetrics.cellSize(),
+                        this.gridMetrics.offsetY() + r * this.gridMetrics.cellSize(),
+                        this.gridMetrics.cellSize(),
+                        this.gridMetrics.cellSize());
             }
         }
 
         // optional grid, for now mandatory to see that code works (due to only white cells)
         this.gc.setStroke(Color.GRAY);
         this.gc.setLineWidth(0.5);
-        for (int r = 0; r <= rows; r++) {
-            this.gc.strokeLine(offsetX, offsetY + r * cellSize, offsetX + totalGridWidth, offsetY + r * cellSize);
+        for (int r = 0; r <= this.gridMetrics.rows(); r++) {
+            this.gc.strokeLine(
+                    this.gridMetrics.offsetX(),
+                    this.gridMetrics.offsetY() + r * this.gridMetrics.cellSize(),
+                    this.gridMetrics.offsetX() + this.gridMetrics.totalGridWidth(),
+                    this.gridMetrics.offsetY() + r * this.gridMetrics.cellSize());
         }
-        for (int c = 0; c <= cols; c++) {
-            this.gc.strokeLine(offsetX + c * cellSize, offsetY, offsetX + c * cellSize, offsetY + totalGridHeight);
+        for (int c = 0; c <= this.gridMetrics.cols(); c++) {
+            this.gc.strokeLine(
+                    this.gridMetrics.offsetX() + c * this.gridMetrics.cellSize(),
+                    this.gridMetrics.offsetY(),
+                    this.gridMetrics.offsetX() + c * this.gridMetrics.cellSize(),
+                    this.gridMetrics.offsetY() + this.gridMetrics.totalGridHeight());
         }
 
 
